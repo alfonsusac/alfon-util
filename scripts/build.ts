28ca2b4060ce8d1dbc0077314674408769ecbce3
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-25 11:20:17 +0700

    test build 13

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,4 +4,7 @@ import { build_next_in_vercel_with_discord_log } from "@/content/workflow/discor
 
 await build_next_in_vercel_with_discord_log({
   webhook_url: process.env.DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL,
+  cmds: [
+    ['next', 'build']
+  ],
 })
--- src/content/workflow/discord-logging.ts
+++ src/content/workflow/discord-logging.ts
@@ -6,34 +6,20 @@ import { maskedlink, post_discord_webhook } from "../utils/util-discord-webhook"
 
 export async function build_next_in_vercel_with_discord_log(args: {
   webhook_url?: string,
-  prebuild?: () => Promise<void> | void
+  cmds: string[][],
 }) {
   const build_env = process.env as VERCEL_BUILD_ENV & {
     DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL?: string
   }
 
-  const log_header = () => {
-    if (build_env.VERCEL !== '1') return 'Not in Vercel environment'
-    const v_team = "alfonsusacs-projects"
-    const deployment_link = `https://vercel.com/${ v_team }/${ build_env.VERCEL_PROJECT_NAME }/${ build_env.VERCEL_DEPLOYMENT_ID.replace('dpl_', '') }`
-    return '-# ' + maskedlink(build_env.VERCEL_PROJECT_NAME, deployment_link)
-      + ' - ' + build_env.VERCEL_ENV
-      + ' - ' + maskedlink(`${ build_env.VERCEL_GIT_COMMIT_REF }-${ build_env.VERCEL_GIT_COMMIT_SHA.slice(0, 7) }`, `https://github.com/${ build_env.VERCEL_GIT_REPO_SLUG }/commit/${ build_env.VERCEL_GIT_COMMIT_SHA }`)
-      + ' - ' + build_env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN
-  }
-
-  async function post_log(...message: string[]) {
-    if (build_env.VERCEL === '1')
-      await post_discord_webhook(
-        build_env.DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL!,
-        message.join("\n"),
-      )
-  }
-
-  try {
-    if (build_env.VERCEL === '1')
-      if (!build_env.DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL)
-        throw new Error("DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL is not set")
+  async function run_command_with_logs(cmd: string[]): Promise<{
+    exit_code: number,
+    duration: number,
+    success: boolean,
+    log_joined: string,
+    log_lines: string[],
+    log_chunks: string[],
+  }> {
 
     const start = performance.now()
     const proc = Bun.spawn({
@@ -68,20 +54,56 @@ export async function build_next_in_vercel_with_discord_log(args: {
     const exitCode = await proc.exited
     const duration = performance.now() - start
     const success = exitCode === 0
-
-    console.log(`Build finished in ${ (duration / 1000).toFixed(2) }s`)
-    // 0 -> success
-    // 1 -> failure   
-
     const lines = logs.flatMap(log => log.split('\n'))
-    const line_count = lines.length
-    const joined_logs = logs.join('')
-    const success_badge = success
-      ? '<:checkl:1552880098846449784>'
-      : '<:crossl:1552875846241357834>'
 
-    // *sigh* has to await for this before process return so the fetch actually went through
-    await (async () => {
+    return {
+      exit_code: exitCode,
+      duration,
+      success,
+      log_joined: logs.join(''),
+      log_chunks: logs,
+      log_lines: lines,
+    }
+  }
+
+  const log_header = () => {
+    if (build_env.VERCEL !== '1') return 'Not in Vercel environment'
+    const v_team = "alfonsusacs-projects"
+    const deployment_link = `https://vercel.com/${ v_team }/${ build_env.VERCEL_PROJECT_NAME }/${ build_env.VERCEL_DEPLOYMENT_ID.replace('dpl_', '') }`
+    return '-# ' + maskedlink(build_env.VERCEL_PROJECT_NAME, deployment_link)
+      + ' - ' + build_env.VERCEL_ENV
+      + ' - ' + maskedlink(`${ build_env.VERCEL_GIT_COMMIT_REF }-${ build_env.VERCEL_GIT_COMMIT_SHA.slice(0, 7) }`, `https://github.com/${ build_env.VERCEL_GIT_REPO_SLUG }/commit/${ build_env.VERCEL_GIT_COMMIT_SHA }`)
+      + ' - ' + build_env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN
+  }
+
+  async function post_log(...message: string[]) {
+    if (build_env.VERCEL === '1')
+      await post_discord_webhook(
+        build_env.DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL!,
+        message.join("\n"),
+      )
+  }
+
+  try {
+    if (build_env.VERCEL === '1')
+      if (!build_env.DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL)
+        throw new Error("DISCORD_VERCEL_BUILD_LOG_WEBHOOK_URL is not set")
+
+    for (const cmd of args.cmds) {
+      const res = await run_command_with_logs(cmd)
+
+      const {
+        duration, log_lines: lines, success, log_joined
+      } = res
+
+      console.log(`Build finished in ${ (duration / 1000).toFixed(2) }s`)
+
+      const line_count = lines.length
+      const success_badge = success
+        ? '<:checkl:1552880098846449784>'
+        : '<:crossl:1552875846241357834>'
+
+      // *sigh* has to await for this before process return so the fetch actually went through
       await post_log(
         log_header(),
         `-# "${ build_env.VERCEL_GIT_COMMIT_MESSAGE }"`,
@@ -89,10 +111,11 @@ export async function build_next_in_vercel_with_discord_log(args: {
         `${ success_badge }Build Logs`,
         `-# ${ line_count } lines - ${ (duration / 1000).toFixed(2) }s`,
         '\`\`\`ansi',
-        joined_logs.length > 1500 ? `${ joined_logs.slice(0, 1500) + '...' }` : `${ joined_logs }`,
+        log_joined.length > 1500 ? `${ log_joined.slice(0, 1500) + '...' }` : `${ log_joined }`,
         '\`\`\`',
       )
-      if (success)
+      if (success) {
+
         await post_log(
           `-# ​`,
           "Domains",
@@ -111,9 +134,16 @@ export async function build_next_in_vercel_with_discord_log(args: {
           `-# ​`,
           `-# ​`,
         )
-    })()
+      }
 
-    process.exitCode = exitCode
+      if (res.exit_code !== 0) {
+        process.exitCode = res.exit_code
+        return
+      }
+    }
+
+
+
     return
   } catch (error) {
     if (error !== 'exit-code-1') {
@@ -129,7 +159,10 @@ export async function build_next_in_vercel_with_discord_log(args: {
         error_stack_section
       )
     }
-    // return 1
     throw error
   }
 }
+
+
+
+
